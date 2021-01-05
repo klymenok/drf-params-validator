4d4b313cbbf68a567fa16bcf543de523a425b564
Oleksii Klymenok <ok@ucode.world>
2021-01-05 22:39:42 +0200

    update docstring; error fix

--- drf_action_params_validator/__init__.py
+++ drf_action_params_validator/__init__.py
@@ -3,10 +3,12 @@ from django.utils.functional import wraps
 from rest_framework.response import Response
 from rest_framework import status
 
+
 def validate_request_params(params=None, data=None):
     """
     The decorator for view actions to validate request parameters presence
-    :param params: list of params names to validate
+    :param params: list of GET params names to validate
+    :param params: list of POST data items to validate
     :return:
     """
     def deco(f):
@@ -14,7 +16,7 @@ def validate_request_params(params=None, data=None):
         def wrapper(self, request, *args, **kwargs):
             if data:
                 for item in data:
-                    if not request.data.get(data):
+                    if not request.data.get(item):
                         return Response(f'`{item}` parameter is missing', status=status.HTTP_400_BAD_REQUEST)
             if params:
                 for param in params:
